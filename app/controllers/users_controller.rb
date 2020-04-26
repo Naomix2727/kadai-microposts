f812137aabf5f1a9614f0d780b030d8e8df5288e
Naoya.Ichiakwa <naomix27@gmail.com>
2020-04-26 01:39:25 +0000

    add favorite_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,9 +39,16 @@ class UsersController < ApplicationController
     counts(@user)
   end
 
+  def like
+    @user = User.find(params[:id])
+    @microposts = @user.favoritings.order(id: :desc).page(params[:page])
+    counts(@user)
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
